d51e37227e5d8de15b416f95698c78c08c065a33
unuefeejovwoke <vuwill7114@gmail.com>
2022-06-27 20:38:45 -0700

    add to cart modal pop up

--- js/app.js
+++ js/app.js
@@ -2,8 +2,13 @@ const menu = document.querySelector('.menu');
 const btnHamburger = document.querySelector('.hamburger');
 const btnMenuClose = document.querySelector('#btnMenuClose');
 
+const cart = document.querySelector('.cart');
+const btnCart = document.querySelector('.btncart');
+
 btnHamburger.addEventListener('click', onHamburgerClick);
 btnMenuClose.addEventListener('click', onBtnMenuCloseClick);
+
+btnCart.addEventListener('click', openCart);
 
 function onHamburgerClick() {
     menu.classList.remove('hidden');
@@ -11,4 +16,8 @@ function onHamburgerClick() {
 
 function onBtnMenuCloseClick() {
     menu.classList.add('hidden');
+}
+
+function openCart() {
+    cart.classList.toggle('hidden');
 }
